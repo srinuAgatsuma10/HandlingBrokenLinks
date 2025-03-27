05f7b904b631bf056cfc8da7a6c2059a0f873a4d
Giridhara Srinivas <ggsrinivascm035@gmail.com>
2025-03-27 20:26:10 +0530

    automationBlogSpot set up

---
+++ src/test/java/automaionBlogSpot/HandlingBrokenLinks.java
@@ -0,0 +1,32 @@
+package automaionBlogSpot;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class HandlingBrokenLinks {
+	WebDriver driver;
+
+	@BeforeClass
+	public void setUp() {
+		driver = new ChromeDriver();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get("https://testautomationpractice.blogspot.com/");
+		driver.manage().window().maximize();
+	}
+	
+	@Test
+	public void brokenLinks() {
+		
+	}
+	
+	@AfterClass
+	public void tearDown() {
+		driver.quit();
+	}
+}
