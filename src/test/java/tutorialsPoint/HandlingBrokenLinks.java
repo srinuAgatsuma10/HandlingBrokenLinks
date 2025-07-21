3a914874bbacf37b88eda77f0285ea60a02f3d57
Giridhara Srinivas <ggsrinivascm035@gmail.com>
2025-07-21 19:05:57 +0530

    tutorialsPoint final commit

--- src/test/java/tutorialsPoint/HandlingBrokenLinks.java
+++ src/test/java/tutorialsPoint/HandlingBrokenLinks.java
@@ -1,8 +1,13 @@
 package tutorialsPoint;
 
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.time.Duration;
+import java.util.List;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -23,7 +28,36 @@ public class HandlingBrokenLinks {
 
 	@Test
 	public void handlingBrokenLinks() {
+		// Capture all link elements in webpage
+		List<WebElement> links = driver.findElements(By.tagName("a"));
+		System.out.println("Number of links : " + links.size());
 
+		int brokenLinks = 0;
+
+		// Access links from the elements
+		for (WebElement linkElement : links) {
+			String hrefValue = linkElement.getAttribute("href");
+			if (hrefValue == null || hrefValue.isEmpty()) {
+				System.out.println("href attribute is empty, not possible to check");
+				continue;
+			}
+
+			// Hit URL to the Server
+			try {
+				URL url = new URL(hrefValue); // convert href value from string to URL
+				HttpURLConnection con = (HttpURLConnection) url.openConnection(); // open connection to the server
+				con.connect(); // connect to the server and send request to the server
+
+				if (con.getResponseCode() >= 400) {
+					System.out.println(hrefValue + "========> Broken Link");
+					brokenLinks++;
+				} else {
+					System.out.println(hrefValue + "========> Not a Broken Link");
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
 	}
 
 	@AfterClass()
